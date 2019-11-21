ca7b039df6b77316737851ce51dad6b6666683ea
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2019-11-21 20:31:27 +0100

    testeando script en  object para coger elementos del dom y del DOM HTML

---
+++ js/foo.js
@@ -0,0 +1,4 @@
+let elementoTest = document.getElementById("path10787-88");
+console.log(elementoTest);
+let svg = document.getElementsByClassName("mapa").item(2);
+console.log(document.ownerDocument);
